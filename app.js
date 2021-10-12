60d77a65bc48af383a552fcc039611d9805dba08
BradMitchell <32003566+MitcBrad@users.noreply.github.com>
2021-10-12 17:05:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let stringers=[3, "Jerry", "Sandy", 4, "Pepper", 15, "Jocastah", 12];
+let array = document.createElement("div");
+array.innerHTML = "The array is: " + stringers;
+document.body.appendChild(array);
+for(let i = 0; i<stringers.length; i++){
+    let newdiv = document.createElement("div");
+    if(isNaN(stringers[i])) {
+        if ((stringers[i]).length > 5) {
+            let value = "the length of the string is more than 5.";
+            newdiv.innerHTML = "The value is " + stringers[i] + " and " + value;
+            document.body.append(newdiv);
+        } else {
+            let value = " it's normal."
+            newdiv.innerHTML = "The value is " + stringers[i] + " and " + value;
+            document.body.append(newdiv);
+        }
+    }
+    else{
+        if(stringers[i] < 10){
+            let value = "the number is below 10.";
+            newdiv.innerHTML = "The value is " + stringers[i] + " and " + value;
+            document.body.append(newdiv);
+        }
+        else if(stringers[i] == 15){
+            let value = "the number is 15.";
+            newdiv.innerHTML = "The value is " + stringers[i] + " and " + value;
+            document.body.append(newdiv);
+        }
+        else{
+            let value = "it's normal.";
+            newdiv.innerHTML = "The value is " + stringers[i] + " and " + value;
+            document.body.append(newdiv);
+        }
+    }
+
+
+
+
+
+
+}
+// If it is number below 10.
+// If the string's character length is more than 5
+// If the number is exactly 15
+// If none of the above, say "normal"
